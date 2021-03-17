69b12eaf5592315b1aaf61a000385bcfcf21f52b
Christopher <drakochris1992@gmail.com>
2021-03-17 15:11:39 -0400

    finished set up

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'School Project' });
+});
+router.get('/h', function (req, res, next) {
+  res.render('index', { title: 'H' });
+});
+router.get('/users', function (req, res, next) {
+  res.render('users');
 });
 
 module.exports = router;
